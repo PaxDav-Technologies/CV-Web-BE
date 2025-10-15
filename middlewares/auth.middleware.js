811f56f5d35de8d5d6001b29ea4cd183e8f24ec4
Oluwayimika <oladipupomichael9@gmail.com>
2025-10-15 19:56:36 +0100

    feat: auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+exports.authenticate = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return res
+        .status(401)
+        .json({ message: `Authentication token is required` });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: `Invalid token` });
+    }
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    if (!user) {
+      return res.status(401).json({ message: `Unauthorized` });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: `Internal Server Error` });
+  }
+};
+
+exports.authorizeRoles = async () => {};
